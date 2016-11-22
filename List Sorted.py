c086debe00a8fdb59919942597002945c0812f40
peaches12 <vlaz1999@yahoo.com>
2016-11-21 22:04:09 -0500

    Commit_olga_Nov21

--- List Sorted.py
+++ List Sorted.py
@@ -1,24 +1,22 @@
-# the program will take input names from the user, creates a list and 
-# created a new list of the sorted names; displays the both lists
+#the program will determine if a list is sorted one or not
 
-num = input("How many names do you want to enter? Give a number")
+list1 = [2,5,1,0,-6,12,7]
+list2 = list(range(0,6))
+list3 = ['a','b','A','d']
+list4 = ['Mary', 'John', 'Teresa', 'Angela']
+print("Call the function 'sorted_list(parameter)' ")
+print("Choose list1/list2/list3/list4 as the parameter")
+print()
 
+# create a function to determine, if the passed list is a sorted list
 
-name_list = []
-
-name = input("Enter a name: ")
-name_list.pop(name)
-
-
-choice = input("Continue? yes/no: ")
-
-while choice.upper() == "YES"  and m <= num :
-	name = input("Enter another name: ")
-	name_list.pop(name)
-	m = m + 1
-	choice = input("Continue? yes/no:  ")
-	print()
-
-for m in name_list() : 
-	print(name_list[m])
+def sorted_list(list):
+        if list == sorted(list):
+                print('the list is sorted')
+                print(list)
+        else:
+                print('the list is not sorted: ' + str(list))
+                print()
+                print('after sorting the list: ' + str(sorted(list)))
+                
 
